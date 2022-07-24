9c4718c8e7e3c491494bf4a281b57143f22f15dd
Arsh Khatwani <arsh.khatwani@gmail.com>
2022-07-24 16:53:02 +0530

    Update upload middleware

--- server/middlewares/bookUpload.js
+++ server/middlewares/bookUpload.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 
-const multerFilter = (req, file, cb) => {
+const bookFilter = (req, file, cb) => {
   if (file.mimetype.split("/")[1] === "pdf") {
     cb(null, true);
   } else {
@@ -9,13 +9,35 @@ const multerFilter = (req, file, cb) => {
   }
 };
 
+const bookCoverFilter = (req, file, cb) => {
+  if (
+    file.mimetype.split("/")[1] === "png" ||
+    file.mimetype.split("/")[1] === "jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Not an Image File!"), false);
+  }
+};
+
+const multerFilter = (req, file, cb) => {
+  if (file.fieldname === "bookFile") return bookFilter(req, file, cb);
+
+  if (file.fieldname === "bookCoverFile") return bookCoverFilter(req, file, cb);
+};
+
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "books");
+    if (file.fieldname === "bookFile") cb(null, "books");
+    else if (file.fieldname === "bookCoverFile") cb(null, "bookCovers");
   },
+
   filename: (req, file, cb) => {
     const ext = file.mimetype.split("/")[1];
-    const bookId = uuidv4();
+
+    const bookId = req.savedBookId !== undefined ? req.savedBookId : uuidv4();
+    req.savedBookId = bookId;
+
     cb(null, `${bookId}.${ext}`);
   },
 });
