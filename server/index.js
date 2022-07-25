d59b2e2fa47adacd93d3803017063dd5a13c5aa1
Arsh Khatwani <arsh.khatwani@gmail.com>
2022-07-25 23:29:35 +0530

    Add cors in server

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const db = require("./db.config");
 const bookRoutes = require("./routes/bookRoutes");
+const cors = require("cors");
 
 db.connect();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
